22d8e3916bbd64c483bf1391f0550a8e826eae34
Zahra Bahmanpour <z.bmnpr@gmail.com>
2024-01-14 23:36:41 +0330

    add noStore

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,9 +9,11 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
+  noStore();
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
   try {
